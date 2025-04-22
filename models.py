55a9ed605d61a155c5244a84f27b874de2bce91b
elixxare <elina.liuyx@gmail.com>
2025-04-22 04:26:06 -0400

    add friends model

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import relationship
 db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
     username = db.Column(db.Text, primary_key=True)
-    uname = db.Column(db.Text, nullable=False)
+    uname = db.Column(db.Text, nullable=False) #first last name
     upassword = db.Column(db.Text, nullable=False)
     bio = db.Column(db.Text)
     email = db.Column(db.Text)
@@ -33,3 +33,13 @@ class Cookbook(db.Model):
     __tablename__ = 'cookbook'
     author = db.Column(db.Text, primary_key=True)
     recipeid = db.Column(db.Text, primary_key=True)
+
+class Friends(db.Model):
+    __tablename__ = 'friends'
+    id = db.Column(db.Integer, primary_key=True) #friendship id
+    user_id = db.Column(db.Text, db.ForeignKey('user.username')) 
+    friend_id = db.Column(db.Text, db.ForeignKey('user.username'))
+    status = db.Column(db.String(20), default = 'pending') #add functionality to request friendships
+    
+    user = db.relationship('User', foreign_keys=[user_id], backref = 'sent_requests')
+    friend = db.relationship('User', foreign_keys=[friend_id], backref = 'received_requests')
